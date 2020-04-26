11a6d2566da0bd869bc8684e6611c4113a639432
Neelansh <neelansh.s@bpunceshare.com>
2020-04-26 23:26:57 +0530

    changed name

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:headphone_strore/screens/brand_names.dart';
 import 'package:headphone_strore/utils/colors.dart';
 import 'package:headphone_strore/widgets/brand_card.dart';
 
@@ -8,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Headphone Store',
       themeMode: ThemeMode.dark,
       home: BrandNames(),
     );
